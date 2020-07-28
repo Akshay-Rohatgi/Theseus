c9e5ead27034360643d04a9a78f8e51669fe0156
Akshay-Rohatgi <52616034+Akshay-Rohatgi@users.noreply.github.com>
2020-07-27 21:41:48 -0700

    need license and will publish

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Theseus
 Quick python formatter for the Sarpedon Aeacus Scoring Endpoint configuration file.
+
+## Examples
+An example output configuration file in /ouput with a corresponding csv in /csvs is available
--- theseus.py
+++ theseus.py
@@ -2,6 +2,20 @@ from pathlib import Path
 import pandas
 import os
 
+print('''
+
+============================================================================
+ooooooooooooo oooo                                                         
+8'   888   `8 `888                                                          
+     888       888 .oo.    .ooooo.   .oooo.o  .ooooo.  oooo  oooo   .oooo.o 
+     888       888P"Y88b  d88' `88b d88(  "8 d88' `88b `888  `888  d88(  "8 
+     888       888   888  888ooo888 `"Y88b.  888ooo888  888   888  `"Y88b.  
+     888       888   888  888    .o o.  )88b 888    .o  888   888  o.  )88b 
+    o888o     o888o o888o `Y8bod8P' 8""888P' `Y8bod8P'  `V88V"V8P' 8""888P'
+============================================================================
+
+''')
+
 # get user input for settings
 handle = open("output/final.conf", "w")
 
